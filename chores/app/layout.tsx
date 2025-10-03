a551623c4f4962c420a1bb9350a024dd662da3bf
Barak Raskin <{email}>
2025-10-03 15:08:26 +0300

    test

--- chores/app/layout.tsx
+++ chores/app/layout.tsx
@@ -9,8 +9,8 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata: Metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  title: "chores app",
+  description: "We'll see where this is going",
 };
 
 const geistSans = Geist({
