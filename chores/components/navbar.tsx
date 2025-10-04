75fbf7cab38ce11f35abf2a3979d89dd47b2139a
raskinbarak <barak.raskin@gmail.com>
2025-10-04 23:22:56 +0300

    logo

--- chores/components/navbar.tsx
+++ chores/components/navbar.tsx
@@ -1,12 +1,28 @@
+"use client";
+
 import Link from "next/link";
 import React from "react";
 import { ThemeSwitcher } from "./theme-switcher";
+import Image from "next/image";
+import { useTheme } from "next-themes";
 
 export const Navbar = () => {
+  const { resolvedTheme } = useTheme();
+
   return (
     <nav className="w-full border-b border-b-foreground/10 bg-background/50 backdrop-blur-sm">
       <div className="w-full max-w-5xl mx-auto px-4 sm:px-6 h-16 flex items-center justify-between">
         <div className="flex items-center gap-4">
+          {resolvedTheme === "dark" ? (
+            <Image src="/darkicon.png" width={40} height={40} alt="dark icon" />
+          ) : (
+            <Image
+              src="/lighticon.png"
+              width={40}
+              height={40}
+              alt="light icon"
+            />
+          )}
           <Link href={"/"} className="text-lg font-semibold tracking-tight">
             Trying To Code
           </Link>
